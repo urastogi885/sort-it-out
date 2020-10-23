51d5acaecba3a274480d585fdaabed9687c6872b
umang <umang.rastogi96@gmail.com>
2020-10-23 13:17:25 -0400

    [sort.cpp] Implement methods for merge-sort

--- src/sort.cpp
+++ src/sort.cpp
@@ -7,7 +7,7 @@
 
 #include <sort.hpp>
 
-void Sort::insertion(std::vector<int> &arr) {
+void Sort::insertion_sort(std::vector<int> &arr) {
 	// Define type of variables
 	int temp;
 	int i;
@@ -20,4 +20,49 @@ void Sort::insertion(std::vector<int> &arr) {
 		}
 		arr[i + 1] = temp;
 	}
+}
+
+void Sort::merge_sort(std::vector<int> &arr, int p, int r) {
+	if (p < r) {
+		int q = (p + r) / 2;
+		merge_sort(arr, p, q);
+		merge_sort(arr, q + 1, r);
+		merge(arr, p, q, r);
+	}
+}
+
+void Sort::merge(std::vector<int> &arr, int p, int q, int r) {
+	int n1 = q - p + 1;
+	int n2 = r - q;
+	std::vector<int> l_arr;
+	std::vector<int> r_arr;
+
+	for (int i = 0; i < n1; i++) {
+		l_arr.push_back(arr[p + i]);
+	}
+	for (int i = 0; i < n2; i++) {
+		r_arr.push_back(arr[q + i + 1]);
+	}
+	int i = 0;
+	int j = 0;
+	int k = p;
+	//marge temp arrays to real array
+   while(i < n1 && j < n2) {
+      if(l_arr[i] <= r_arr[j]) {
+         arr[k] = l_arr[i];
+         i++;
+      }else{
+         arr[k] = r_arr[j];
+         j++;
+      }
+      k++;
+   }
+   while(i < n1) {       //extra element in left array
+      arr[k] = l_arr[i];
+      i++; k++;
+   }
+   while(j < n2) {     //extra element in right array
+      arr[k] = r_arr[j];
+      j++; k++;
+   }
 }
