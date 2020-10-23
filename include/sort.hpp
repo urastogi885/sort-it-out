73f0e8725ca4de0553cbe28e84686909a1a1c08b
umang <umang.rastogi96@gmail.com>
2020-10-23 13:16:55 -0400

    [sort.hpp] Add methods for merge-sort

--- include/sort.hpp
+++ include/sort.hpp
@@ -9,6 +9,7 @@
 #define INCLUDE_SORT_HPP_
 
 #include <vector>
+#include <algorithm>
 
 class Sort {
 private:
@@ -21,7 +22,23 @@ public:
 	* @param pointer to the array
 	* @return none
 	*/
-	void insertion(std::vector<int> &arr);
+	void insertion_sort(std::vector<int> &arr);
+
+	/*
+	* @brief sort an array using insertion-sort
+	* @param pointer to the array
+	* @param sub-array ending index
+	* @param sub-array starting index
+	* @return none
+	*/
+	void merge_sort(std::vector<int> &arr, int r, int p);
+
+	/*
+	* @brief merge 2 sorted sub-arrays
+	* @param pointer to the array
+	* @return none
+	*/
+	void merge(std::vector<int> &arr, int p, int q, int r);
 };
 
 #endif // INCLUDE_SORT_HPP_
