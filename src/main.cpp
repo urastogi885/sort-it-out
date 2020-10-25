20bf799381e9728fb283ebf477a961cbbe3afccd
umang <umang.rastogi96@gmail.com>
2020-10-25 13:00:58 -0400

    [main.cpp] Use heap-sort
    Use std namespace

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,21 @@
 #include <iostream>
 #include <sort.hpp>
 
-void print(std::vector<int> const &arr);
+void print(vector<int> const &arr);
 
 int main() {
-	std::vector<int> array_to_sort{5, 2, 4, 6, 1, 3};
+	vector<int> array_to_sort{5, 2, 4, 6, 1, 3};
 	Sort sort;
-	sort.bubble_sort(array_to_sort);
+	sort.heap_sort(array_to_sort);
 	print(array_to_sort);
 	return 0;
 }
 
-void print(std::vector<int> const &arr) {
-	std::cout << "The vector elements are : ";
+void print(vector<int> const &arr) {
+	cout << "The vector elements are : ";
 
    	for(int i = 0; i < int(arr.size()); i++) {
-   		std::cout << arr[i] << ' ';
+   		cout << arr[i] << ' ';
    	}
-   	std::cout << '\n';
+   	cout << '\n';
 }
