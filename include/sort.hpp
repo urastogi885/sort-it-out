24c52a903426a856869e08e8b452dc9a92d48c9e
umang <umang.rastogi96@gmail.com>
2020-10-25 12:59:22 -0400

    [sort.hpp] Add function definitions for heap-sort

--- include/sort.hpp
+++ include/sort.hpp
@@ -9,11 +9,17 @@
 #define INCLUDE_SORT_HPP_
 
 #include <vector>
-#include <algorithm>
+
+using namespace std;
 
 class Sort {
 private:
 public:
+	/*
+	* @brief constructor for the class
+	* @param none
+	* @return none
+	*/
 	Sort() {
 	}
 
@@ -22,15 +28,16 @@ public:
 	* @param pointer to the array
 	* @return none
 	*/
-	void insertion_sort(std::vector<int> &arr);
+	void insertion_sort(vector<int> &arr);
 
 	/*
 	* @brief sort an array using bubble-sort
 	* @param pointer to the array
 	* @return none
 	*/
-	void bubble_sort(std::vector<int> &arr);
+	void bubble_sort(vector<int> &arr);
 
+	///////// Declarations for Merge-Sort /////////
 	/*
 	* @brief sort an array using insertion-sort
 	* @param pointer to the array
@@ -38,14 +45,37 @@ public:
 	* @param sub-array starting index
 	* @return none
 	*/
-	void merge_sort(std::vector<int> &arr, int r, int p);
+	void merge_sort(vector<int> &arr, int r, int p);
 
 	/*
 	* @brief merge 2 sorted sub-arrays
 	* @param pointer to the array
 	* @return none
 	*/
-	void merge(std::vector<int> &arr, int p, int q, int r);
+	void merge(vector<int> &arr, int p, int q, int r);
+
+	///////// Declarations for Heap-Sort /////////
+	/*
+	* @brief sort an array using heap-sort
+	* @param pointer to the array
+	* @return none
+	*/
+	void heap_sort(vector<int> &arr);
+
+	/*
+	* @brief build heap
+	* @param pointer to the array
+	* @return none
+	*/
+	void build_max_heap(vector<int> &arr, int size);
+
+	/*
+	* @brief method to maintain the max-heap property
+	* @param pointer to the array
+	* @param largest index
+	* @return none
+	*/
+	void max_heapify(vector<int> &arr, int n, int k);
 };
 
 #endif // INCLUDE_SORT_HPP_
